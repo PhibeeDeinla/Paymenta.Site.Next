95df20b89e904d482e65de9914daf7846f73f095
PhibeeDeinla <phibee.deinla@dssctech.com>
2024-06-11 17:50:00 +0400

    refactor: incorrect naming

--- components/sections/services.section.tsx
+++ components/sections/services.section.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import HeaderTitle from '../headerTitle'
 import type { TServicesProps } from '@/types/types'
 
-const featuresList: TServicesProps[] = [
+const servicesList: TServicesProps[] = [
     {
         title: 'Development & Design',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie euismod ex non bibendum',
@@ -27,7 +27,10 @@ const ServicesSection = (props: Props) => {
             <div className="max-w-screen-2xl mx-auto h-full">
                 <div className="grid grid-cols-3 gap-4 h-full items-center">
                     <div className="col-span-1">
-                        <HeaderTitle label="Services" className='py-5'/>
+                        <HeaderTitle
+                            label="Services"
+                            className="py-5"
+                        />
 
                         <p className="px-5 font-thin tracking-wide pt-6">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie euismod ex non bibendum. Suspendisse potenti. Ut quis
@@ -37,7 +40,7 @@ const ServicesSection = (props: Props) => {
                     <div className="col-span-2 px-10">
                         {/* Services List */}
                         <ul className="inline-flex lg:space-x-0">
-                            {featuresList.map(({ ...item }) => (
+                            {servicesList.map(({ ...item }) => (
                                 <li className="ease-out duration-300 transition-all group inline-flex flex-col bg-white hover:bg-primary shadow-lg px-8 py-10">
                                     <div className="group-hover:text-white text-primary text-lg pt-4">{item.title}</div>
                                     <div className="font-thin tracking-wide pt-6 group-hover:text-white">{item.description}</div>
